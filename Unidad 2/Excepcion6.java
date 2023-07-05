3e94e32594458eb2b47415df4a498edfe74119ae
Estefanny <fsanscoli@gmail.com>
2023-07-05 14:43:16 -0600

    subi archivo

--- Unidad 2/Excepcion6.java
+++ Unidad 2/Excepcion6.java
@@ -2,13 +2,26 @@ package excepcion6;
 
 public class Excepcion6 {
 
+    public static class NumerosNullException extends Exception {
+        public NumerosNullException(String mensaje) {
+            super(mensaje);
+        }
+    }
+
     public static void main(String[] args) {
         Integer numero1 = null;
         Integer numero2 = 5;
-        
-        Integer resultado = numero1 + numero2; 
-        
-        System.out.println("El resultado de la suma es: " + resultado);
+
+        try {
+            if (numero1 == null || numero2 == null) {
+                throw new NumerosNullException("Error: Uno o ambos números son nulos.");
+            }
+
+            Integer resultado = numero1 + numero2;
+
+            System.out.println("El resultado de la suma es: " + resultado);
+        } catch (NumerosNullException e) {
+            System.out.println(e.getMessage());
+        }
     }
-    
 }
